ad8ee511952082bcf2988c0b9c46efb62b621c7d
Karina1213 <karinaa121213@gmail.com>
2019-12-27 01:12:12 +0200

    correct file

--- homework-11/src/assets/js/sign-up.js
+++ homework-11/src/assets/js/sign-up.js
@@ -1,13 +1,14 @@
-async function SendDateFormLogIn() {
+async function SendDateFormLogIn(e) {
+    e.preventDefault();
     const url = 'http://localhost:3000/api/users';
     let email = document.getElementById("email").value;
     let password = document.getElementById('password').value;
     let name = document.getElementById('name').value;
 
     let user = {
-        "name": "lala",
-        "password": "123455678",
-        "email": "karinaa121213@gmail.com"
+        "name": "name",
+        "password": "password",
+        "email": " email"
     }
     console.log(user);
     try {
@@ -30,8 +31,8 @@ async function SendDateFormLogIn() {
         console.error('Error:', error);
     }
 }
+console.log(response.headers.get('x-auth-token'));
 
-console.log(SendDateFormLogIn())
 
 function setOnClickHandlerByElemId(elemId, callback) {
     let button = document.getElementById(elemId);
